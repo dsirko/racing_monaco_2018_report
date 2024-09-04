a365d3d7662ff16749046168b97414c8aa890ee5
Dmytro Sirko <dimasirko@gmail.com>
2024-09-04 16:40:05 +0300

    code refuctored

--- main.py
+++ main.py
@@ -1,112 +1,35 @@
 import datetime
-from datetime import timedelta
-
-
-def build_report():
-    pass
-
-
-def print_report():
-    pass
 
 
 def read_text_file(file_path):
+    """Reads the contents of a text file."""
     with open(file_path, 'r') as file:
-        contents = file.read()
-    return contents
+        return file.read()
 
 
 def split_data(text_data):
-    split_items = []
-    split_data = text_data.split('\n')
-    for i in range(len(split_data)):
-        split_items.append(split_data[i].split("\n"))
-    for i in range(len(split_items)):
-        split_items[i] = split_items[i][0].split('_')
-    return split_items
+    """Splits the text data into a list of lists."""
+    return [line.split('_') for line in text_data.split('\n') if line]
 
 
-def sort_data(split_items):
-    sort_data = sorted(split_items, key=lambda x: x[0])
-    return sort_data
-
-        
-# work with start log
-text_data_start_log = read_text_file("log/start.log.txt")
-splited_start_log = split_data(text_data_start_log)
-sorted_start_log = sort_data(splited_start_log)
-# print("\nSTART", *sorted_start_log, sep='\n')
+def sort_data(data):
+    """Sorts the data by the first element of each sublist."""
+    return sorted(data, key=lambda x: x[0])
 
 
-# work with end log
-text_data_end_log = read_text_file("log/end.log.txt")
-splited_end_log = split_data(text_data_end_log)
-sorted_end_log = sort_data(splited_end_log)
-# print("\nEND", *sorted_end_log, sep='\n')
+def calculate_time_differences(start_log, end_log):
+    """Calculates the time differences between start and end logs."""
+    time_differences = []
+    for start, end in zip(start_log, end_log):
+        timestamp_start = datetime.datetime.strptime(start[1], '%H:%M:%S.%f')
+        timestamp_end = datetime.datetime.strptime(end[1], '%H:%M:%S.%f')
+        difference = timestamp_end - timestamp_start
+        time_differences.append(difference)
+    return time_differences
 
 
-# work eith abbreviations
-text_data_abbreviations = read_text_file("log/abbreviations.txt")
-splited_abbreviations = split_data(text_data_abbreviations)
-sorted_split_abbreviations = sort_data(splited_abbreviations)
-# print("\nABBREVIATIONS", *sorted_split_abbreviations, sep='\n')
-
-
-# time_res = []
-#
-# for i in sorted_start_log:
-#     datatipe_start = datetime.datetime.strptime(sorted_start_log[i][1], '%H:%M:%S.%f')
-#     datatipe_finish = datetime.datetime.strptime(sorted_end_log[i][1], '%H:%M:%S.%f')
-#     difference = datatipe_finish - datatipe_start
-#     # Extract minutes and seconds
-#     minutes = difference.seconds // 60  # Use floor division to get whole minutes
-#     seconds = difference.seconds % 60  # Use modulo to get remaining seconds
-#     milliseconds = difference.microseconds // 1000  # Get milliseconds
-#
-#     formatted_output = f"{minutes}:{seconds:02}.{milliseconds:03}"
-#     time_res.append(formatted_output)
-#
-# print("test", time_res)
-# # print(type(formatted_output))
-#
-# for i in range(len(sorted_start_log)):
-#     sorted_start_log[i].append(time_res[i])
-
-time_result = []
-
-for i in range(len(sorted_start_log)):
-    timestamp_start_str = sorted_start_log[i][1]
-    timestamp_end_str = sorted_end_log[i][1]
-    # Convert strings to datetime objects
-    timestamp1 = datetime.datetime.strptime(timestamp_start_str, '%H:%M:%S.%f')
-    timestamp2 = datetime.datetime.strptime(timestamp_end_str,'%H:%M:%S.%f')
-    # Calculate the difference
-    difference = timestamp2 - timestamp1
-    minutes, seconds = divmod(difference.seconds, 60)
-    seconds += difference.microseconds / 1e6
-    # Print the difference in seconds
-    time_result.append(difference)
-
-
-
-for abbrev, time in zip(sorted_split_abbreviations, time_result):
-    abbrev.append(time)
-
-prereport = sorted(sorted_split_abbreviations, key=lambda x: x[3])
-
-print(*prereport, sep='\n')
-
-
-
-
-# last_report = []
-# for i in range(len(sorted_split_abbreviations)):
-#     last_report.append(f"{i + 1}. {sorted_split_abbreviations[i][1]} | {sorted_split_abbreviations[i][2]} | {time_result[i]}")
-#     print(last_report[i])
-
-
-# Function to format timedelta to MM:SS.mmm
 def format_timedelta(td):
+    """Formats a timedelta object to MM:SS.mmm format."""
     total_seconds = td.total_seconds()
     minutes = int(total_seconds // 60)
     seconds = int(total_seconds % 60)
@@ -114,10 +37,41 @@ def format_timedelta(td):
     return f"{minutes:02}:{seconds:02}.{milliseconds:03}"
 
 
-# print report
-for i in range(len(prereport)):
-    if i == 15:
-        print("---------------------------------------------------------")
+def build_report(sorted_abbreviations, time_differences):
+    """Combines the sorted abbreviations with the calculated time differences."""
+    report = []
+    for abbrev, time_diff in zip(sorted_abbreviations, time_differences):
+        abbrev.append(time_diff)
+        report.append(abbrev)
+    return sorted(report, key=lambda x: x[3])  # Sort by time difference
 
-    formatted_time = format_timedelta(prereport[i][3])
-    print(f"{i + 1}. {prereport[i][1]} | {prereport[i][2]} | {formatted_time} ")
+
+def print_report(report):
+    """Prints the formatted race report."""
+    for i, entry in enumerate(report):
+        if i == 15:
+            print("---------------------------------------------------------")
+        formatted_time = format_timedelta(entry[3])
+        print(f"{i + 1}. {entry[1]} | {entry[2]} | {formatted_time}")
+
+
+def process_logs():
+    # Process start log
+    start_log = sort_data(split_data(read_text_file("log/start.log.txt")))
+
+    # Process end log
+    end_log = sort_data(split_data(read_text_file("log/end.log.txt")))
+
+    # Process abbreviations
+    abbreviations = sort_data(split_data(read_text_file("log/abbreviations.txt")))
+
+    # Calculate time differences
+    time_differences = calculate_time_differences(start_log, end_log)
+
+    # Build and print the report
+    report = build_report(abbreviations, time_differences)
+    print_report(report)
+
+
+if __name__ == "__main__":
+    process_logs()
